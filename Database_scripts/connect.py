f28e0ce6cb0a03f1053a699992a06cfd3be3c27c
sannagrosoy <108658771+sannagrosoy@users.noreply.github.com>
2022-07-05 14:52:56 +0200

    Split into two files
    Split the one connection file into two files, config and connect

--- Database_scripts/connect.py
+++ Database_scripts/connect.py
@@ -1,25 +1,6 @@
 import psycopg2
 import config
-from configparser import ConfigParser
 import os
-
-
-def config(filename='database.ini', section='postgresql'):
-    # create a parser
-    parser = ConfigParser()
-    # read config file
-    parser.read(filename)
-
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return db
 
 def connect(filename='database.ini', section='postgresql'):
     conn = None
@@ -54,4 +35,3 @@ def connect(filename='database.ini', section='postgresql'):
 
 if __name__ == "__main__":
     connect()
-
