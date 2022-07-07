f463964a202b8312939490a53d08458807bf3782
sannagrosoy <108658771+sannagrosoy@users.noreply.github.com>
2022-07-07 13:07:15 +0200

    Made one class for all the database files
    Put config.py, connect.py and create.py into database.py

---
+++ database_filer/database.py
@@ -0,0 +1,166 @@
+import psycopg2
+from config import config
+import os
+from configparser import ConfigParser
+
+class Database:
+
+    def __init__(self):
+        self.filename='database.ini'
+        self.conn = None
+        self.section='postgresql'
+    
+    def connect(self):
+        try:
+            params = config()
+
+            print("Connecting to the PostgreSQL database...")
+            self.conn = psycopg2.connect(
+                host="localhost",
+                database="postgres",
+                user=os.environ["database_user"],
+                password=os.environ["database_password"])
+            
+            cur = self.conn.cursor()
+            
+            print("PostgreSQL database version:")
+            cur.execute("SELECT version()")
+
+            db_version = cur.fetchone()
+            print(db_version)
+
+            cur.close()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+
+        finally:
+            if self.conn is not None:
+                self.conn.close()
+                print("Database connection closed.")
+
+    def config(self):
+        section=self.section
+        parser = ConfigParser()
+        parser.read(self.filename)
+
+        # get section, default to postgresql
+        db = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                db[param[0]] = param[1]
+                print(param[0], param[1])
+            print('Database configurated')
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(self.section, self.filename))
+
+        return db
+
+    def create_tables(self):
+
+        commands = (
+
+            """
+            CREATE TABLE address (
+                org_address VARCHAR(255) PRIMARY KEY,
+                org_zipcode INTEGER,
+                org_city VARCHAR(255)
+            )
+            """,
+            
+            """
+            CREATE TABLE smb (
+                org_nr INTEGER PRIMARY KEY,
+                org_name VARCHAR(255),
+                org_address VARCHAR(255),
+                CONSTRAINT fk_org_address 
+                    FOREIGN KEY (org_address) 
+                        REFERENCES address(org_address)
+            )
+            """,
+
+        
+            """
+            CREATE TABLE location (
+                loc_nr INTEGER PRIMARY KEY,
+                org_nr INTEGER,
+                loc_name VARCHAR(255),
+                loc_capacity INTEGER,
+                CONSTRAINT fk_org_nr 
+                    FOREIGN KEY (org_nr)
+                        REFERENCES smb(org_nr)
+                
+            )
+            """,
+
+            """
+            CREATE TABLE samlonoid_lice (
+                loc_nr INTEGER PRIMARY KEY,
+                lice BOOL,
+                lice_nr INTEGER,
+                lice_limit INTEGER,
+                lice_week INTEGER,
+                live_year INTERVAL,
+                CONSTRAINT fk_loc_nr 
+                    FOREIGN KEY (loc_nr) 
+                        REFERENCES location(loc_nr)
+            )
+            """,
+
+            """
+            CREATE TABLE escapes (
+                loc_nr INTEGER,
+                escape_nr INTEGER,
+                escape_year INTERVAL,
+                escape_week INTEGER,
+                CONSTRAINT fk_loc_nr    
+                    FOREIGN KEY (loc_nr) 
+                        REFERENCES location(loc_nr)
+            )
+            """,
+
+            """
+            CREATE TABLE salmon_death(
+                loc_nr INTEGER,
+                death_nr INTEGER,
+                death_year INTERVAL,
+                CONSTRAINT fk_loc_nr
+                    FOREIGN KEY (loc_nr) 
+                        REFERENCES location(loc_nr)
+            )
+
+            """
+        )
+
+        try:
+            params = config()
+
+            self.conn = psycopg2.connect(
+                host="localhost",
+                database="postgres",
+                user=os.environ["database_user"],
+                password=os.environ["database_password"]
+            )
+            cur = self.conn.cursor()
+
+            for command in commands:
+                cur.execute(command)
+
+            cur.close()
+            self.conn.commit()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            
+        finally:
+            if self.conn is not None:
+                self.conn.close()
+
+
+
+
+database1 = Database()
+database1.connect()
+database1.config()
+database1.create_tables()
