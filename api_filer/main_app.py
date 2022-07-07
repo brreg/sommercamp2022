4fd6d4591052d9f12ad3a89d44d507a391409a53
JakobNH <jakobnhelgoy@gmail.com>
2022-07-07 13:20:37 +0200

    make main_app
    make main_app
    update .gitignore

---
+++ api_filer/main_app.py
@@ -0,0 +1,21 @@
+import threading
+import time
+import logging
+from barentswatch_api import API
+from licedata import LiceData
+from database import Database
+
+class MainApp:
+    
+    def __init__(self, database):
+        self.database = Database()
+    
+    def start_database(self):
+        pass
+     
+     
+def __main__():
+    pass    
+
+if __name__ == __main__():
+    __main__()
