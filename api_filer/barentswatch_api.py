c9bf6656120d405ad3489de6e0ff53500a213342
Ingunn Augdal Fløvig <ingunn.flovig@gmail.com>
2022-07-07 13:11:09 +0200

    Adding licedata container and changed licedata
    Adding licedata container and changed licedata

--- api_filer/barentswatch_api.py
+++ api_filer/barentswatch_api.py
@@ -1,18 +1,12 @@
 import json
+from pickle import FALSE, TRUE
 import requests as r
 from rauth.service import OAuth2Service
 import os
 import pandas as pd
+import licedata as ld
+import licedata_container as ldc
 
-def make_week_dict(week_value_list):
-    # input is a list of dictionaries with week and value
-    week_dict = {}
-    for dct in week_value_list: 
-        week = dct["week"]
-        value = dct["value"]
-        week_dict[week] = value
-    #output is a dictionary with key=week, value=value
-    return week_dict
 
     
 #Get fishhealth data 
@@ -67,23 +61,57 @@ class API:
         
         return escape_json
     
+    def make_week_dict(self, week_value_list):
+        # input is a list of dictionaries with week and value
+        week_dict = {}
+        for dct in week_value_list: 
+            week = dct["week"]
+            value = dct["value"]
+            week_dict[week] = value
+        #output is a dictionary with key=week, value=value
+        return week_dict
+
+    def putlicedataintoobject(self, fishhealthdata) :
+        # input is a dictionary of the form {localityNo: , year: , data: {weeks:values}}
+        licedatalist = []
+        for week in fishhealthdata["data"].keys():
+            # if there are no lice, licebinary = FALSE, otherwise, licebinary = TRUE
+            if (fishhealthdata["data"][week] > 0):
+                licebinary = True
+            else: 
+                licebinary = False
+            licedata = ld.LiceData(fishhealthdata["localityNo"], 
+                                    fishhealthdata["data"][week], 
+                                    licebinary, 
+                                    week, 
+                                    fishhealthdata["year"])
+            licedatalist.append(licedata)
+        #return list of LiceData objects
+        return licedatalist
         
 def __main__(): 
     bapi = API()
     
     #locnrs= bapi.get_locnrs()
-    
-    fishhealthdata = bapi.get_lice_data(45032, 2022)
-    escapedata = bapi.get_escape_data(45032, 2022)
-    
-    print(fishhealthdata)
-    fishhealthdata["data"] = make_week_dict(fishhealthdata["data"])
-    print(fishhealthdata)
-    fishhealthdata.pop("type")
-    print(fishhealthdata)
-    df = pd.DataFrame(fishhealthdata)
+    #escapedata = bapi.get_escape_data(45032, 2022)
 
-    print(df)
+    # gets lice data about one locnr, in a given year, as a dictionary
+    fishhealthdata = bapi.get_lice_data(45032, 2022)
+    fishhealthdata.pop("type")
+
+    # changing form of the week data to a dictionary with weeks as keys and values as values
+    fishhealthdata["data"] = bapi.make_week_dict(fishhealthdata["data"])
+    print(fishhealthdata)
+
+    # put one fishhealthdata record into multiple LiceData objects
+    licedatalist = bapi.putlicedataintoobject(fishhealthdata)
+
+    # put licedata objects into licedata container
+    licedata_container = ldc.LicedataContainer()
+    licedata_container.addLiceDataList(licedatalist)
 
 if __name__ == "__main__":
     __main__()
+
+
+api = API()
--- api_filer/licedata.py
+++ api_filer/licedata.py
@@ -1,8 +1,17 @@
 class LiceData: 
 
-    def __init__(self, locnr, lice_binary, lice_week, lice_year):
+    def __init__(self, locnr, lice_value, lice_binary, lice_week, lice_year):
         self.locnr = locnr
-        self.lice__binary = lice_binary
+        self.lice_value = lice_value
+        self.lice_binary = lice_binary
         self.lice_week = lice_week
         self.lice_year = lice_year
+        
 
+    def print_licedata(self): 
+        print("Printing licedata")
+        print(self.locnr)
+        print(self.lice_value)
+        print(self.lice_binary)
+        print(self.lice_week)
+        print(self.lice_year)
