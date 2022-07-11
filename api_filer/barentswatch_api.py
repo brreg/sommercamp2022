c9c25bb5a63e346b6079a21c8a4f118a94a6e1fb
Ingunn Augdal Fløvig <ingunn.flovig@gmail.com>
2022-07-11 09:25:54 +0200

    fixed naming error

--- api_filer/barentswatch_api.py
+++ api_filer/barentswatch_api.py
@@ -7,7 +7,7 @@ import pandas as pd
 import licedata as ld
 import licedata_container as ldc
 import escapedata as ed
-import EscapedataContainer as edc
+import escapedataContainer as edc
 import random
 
 ## locality with lice data 45032
