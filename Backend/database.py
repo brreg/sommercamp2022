e5b1e32cf10ab2a0b1f324ba4dbe18e7bf697268
Ingunn Augdal Fløvig <ingunn.flovig@gmail.com>
2022-07-15 12:04:24 +0200

    fixed syntax error!

--- Backend/database.py
+++ Backend/database.py
@@ -142,6 +142,14 @@ class Database:
             """,
 
             """
+            CREATE TABLE producers (
+                producer_id INTEGER PRIMARY KEY,
+                producer VARCHAR(255),
+                co2_ekvival FLOAT
+            )
+            """, 
+
+            """
             CREATE TABLE salmon_death (
                 ID SERIAL PRIMARY KEY,
                 loc_nr INTEGER,
@@ -153,6 +161,7 @@ class Database:
             )
 
             """,
+
             """
             CREATE TABLE greenhouse_gas_emissions (
                 ID SERIAL PRIMARY KEY,
@@ -163,24 +172,16 @@ class Database:
                 production FLOAT,
                 co2e_feed FLOAT,
                 co2e_transport FLOAT,
+                CONSTRAINT fk_loc_nr
+                    FOREIGN KEY(loc_nr)
+                        REFERENCES location(loc_nr),
                 CONSTRAINT fk_producer_id
                     FOREIGN KEY(producer_id)
                         REFERENCES producers(producer_id)
-                CONSTRAINT fk_loc_nr
-                    FOREIGN KEY(loc_nr)
-                        REFERENCES location(loc_nr)
+              
             )
             """, 
-            
-            """
-            CREATE TABLE producers (
-                producer_id PRIMARY KEY,
-                producer VARCHAR(255),
-                co2_ekvival FLOAT
-            )
 
-            """, 
-            
             """
             CREATE TABLE key_financial_figures (
                 ID SERIAL PRIMARY KEY,
@@ -192,11 +193,9 @@ class Database:
                 CONSTRAINT fk_org_nr
                     FOREIGN KEY(org_nr)
                         REFERENCES smb(org_nr)
-
             )
+            """,
 
-            """, 
-            
             """
             CREATE TABLE social_figures (
                 ID SERIAL PRIMARY KEY,
@@ -209,7 +208,6 @@ class Database:
                         REFERENCES smb(org_nr)
 
             )
-
             """
         )
 
