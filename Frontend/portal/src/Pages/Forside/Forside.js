fb24d13e701a3557639f0cf822e260fc3ed2f0f1
sannagrosoy <108658771+sannagrosoy@users.noreply.github.com>
2022-07-21 08:15:15 +0200

    Created a search bar for the frontpage

--- Frontend/portal/src/Pages/Forside/Forside.js
+++ Frontend/portal/src/Pages/Forside/Forside.js
@@ -1,39 +1,22 @@
 import React from 'react';
 import "./Forside.css";
-import { useNavigate } from "react-router-dom";
+import SearchBar from '../../Components/Searchbar/Search_bar';
+import OrgData from "./smb.json";
 
 function Forside() {
-
-    const navigate = useNavigate();
-
     return (
         <div className = "forside">
-            <div class = "background_color">
-                <div class = "logo">
+            <div className = "background_color">
+                <div className = "logo">
                     <p>Bærekraftsportalen</p>
                 </div>
             </div>
-
-            <p> Alle virksomheter </p>
-
-            <div className = "bedrift_button">
-                <button onClick={function handleClick(){navigate("/Bedrifter")}}>
-                    <div className = "header">
-                        Sea AS
-                    </div>
-                    <div>
-                        <p>Bedriftsinfo</p>
-                        <p>I Salma AS har som mål å produsere næringsrik og velsmakende sjømat av ypperste kvalitet. 
-                            Vi dekker en femtedel av den globale etterspørselen etter atlantisk oppdrettslaks, og 
-                            drivkraften vår er kontinuerlig innovasjon og viljen til å produsere laks på en 
-                            stadig mer bærekraftig måte.</p>
-                    </div>
-                </button>
-            </div>
+                <div className="container">
+                    <SearchBar placeholder="Søk etter organisasjon..." data={OrgData} />
+                </div>
         </div>
     );
   }
-
 
   
   export default Forside;
