e097b81cd3f693005d89627d2a3a26b050486b27
Ingunn Augdal Fløvig <ingunn.flovig@gmail.com>
2022-07-21 14:38:37 +0200

    adding graph

--- Frontend/portal/src/Pages/Bedrifter_miljo/Bedrifter_miljo.js
+++ Frontend/portal/src/Pages/Bedrifter_miljo/Bedrifter_miljo.js
@@ -1,61 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import "./Bedrifter_miljo.css"
 import Card from '../../Components/Cards/Cards'
 import {useNavigate} from 'react-router-dom'
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js'
-import {Bar} from "react-chartjs-2";
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-);
 
 const Bedrifter_miljo = () => {
-    const [chartData, setChartData] = useState({
-        datasets: [],
-    })
-
-    const [chartOptions, setChartOptions] = useState({})
-
-    useEffect(() => {
-        setChartData({
-            labels: ['john', 'kevin', 'george', 'michael', 'oreo'],
-            datasets: [
-                {
-                    label: "Whom'st let the dogs out",
-                    data: [12, 55, 34, 120, 720],
-                    borderColor: "rgb(53, 162, 235)",
-                    backgroundColor: "rgb(53, 162, 235, 0.4)",
-                },
-            ],
-        });
-        setChartOptions({
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: "top"
-                },
-                title: {
-                    display: true,
-                    text: "Whom'st let the dogs out"
-                }
-            }
-        })
-    }, []);
-
-
+    
     const navigate = useNavigate();
 
     return (
@@ -69,11 +19,11 @@ const Bedrifter_miljo = () => {
 
             <div className="card-">
                 <Card />
-                <Bar options={chartOptions} data={chartData} />
             </div>
         
         </div>
     )  
-}
+
+    }
 
 export default Bedrifter_miljo;
