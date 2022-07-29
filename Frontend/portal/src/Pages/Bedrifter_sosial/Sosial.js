ffae94fcd4b9a8ed0ebf8e3f7ba6bc5785f7a35d
sannagrosoy <108658771+sannagrosoy@users.noreply.github.com>
2022-07-29 13:11:32 +0200

    merge

--- Frontend/portal/src/Pages/Bedrifter_sosial/Sosial.js
+++ Frontend/portal/src/Pages/Bedrifter_sosial/Sosial.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Sosial.css";
 import { useNavigate, useParams } from "react-router-dom";
 import AS_buttons from "../../Components/Buttons/AS_buttons";
@@ -8,10 +8,25 @@ import FramesSosial from "../../Components/Frames/FramesSosial";
 const Sosial = () => {
 
     const {id} = useParams();
+    const [orgname, setOrgname] = useState("")
+    const axios = require('axios')
+
+    useEffect(() => {
+        
+        axios.get(`http://127.0.0.1:5000/orgs/${id}`)
+        .then( res=> {
+            console.log(res.data.data[0].org_name)
+            setOrgname(res.data.data[0].org_name)
+        })
+        .catch( err=> {
+            console.log(err)
+        })
+      
+    }, []);
 
     return (
         <div>
-        <p className="overskrift-sosial-bedrifter">{id}</p>
+        <p className="overskrift-sosial-bedrifter">{orgname}</p>
         <div className="btn-floater">
             <AS_buttons/>
         </div>
