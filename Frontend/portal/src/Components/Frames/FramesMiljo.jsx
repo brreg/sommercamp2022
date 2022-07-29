053da1bebb55d29d9f1bb379a1380ec617a1e76d
sannagrosoy <108658771+sannagrosoy@users.noreply.github.com>
2022-07-29 12:59:39 +0200

    Test

--- Frontend/portal/src/Components/Frames/FramesMiljo.jsx
+++ Frontend/portal/src/Components/Frames/FramesMiljo.jsx
@@ -12,9 +12,9 @@ function FramesMiljo() {
     const [value, setValue] = useState("");
     useEffect(() => {
         (async() => {
-            setValue("eksempel");
+            setValue("eksempel"); // async funksjon
         })();
-    }, [])
+    }, []);
 
 
         return (
@@ -22,19 +22,19 @@ function FramesMiljo() {
             <div>{value}</div>
             <div className="row-frames">
                 <div className="column-frames">
-                    <Frame overskrift= "Dødlighet" nøkkeltall_en={<Nokkeltall id={id}/>} nøkkeltall_to={<Nokkeltall kpi="gjennomsnittlig dødlighet" id={id}/>} graph={<Death org_nr="886813082"/>}/>
+                    <Frame overskrift= "Dødlighet" nøkkeltall_en={<Nokkeltall id={id} kpi="Dødlighet_bedrift"/>} nøkkeltall_to={<Nokkeltall kpi="Dødlighet_bransje" id={id}/>} graph={<Escape org_nr="886813082"/>}/>
                 </div>
                 <div className="column-frames">
-                    <Frame overskrift="Rømninger" nøkkeltall_en={<Nokkeltall id={id}/>} nøkkeltall_to={<Nokkeltall id={id}/>} graph={<Escape org_nr="886813082"/>}/>
+                    <Frame overskrift="Rømninger" nøkkeltall_en={<Nokkeltall id={id} kpi="Rømninger_bedrift"/>} nøkkeltall_to={<Nokkeltall id={id} kpi="Rømninger_bransje"/>} graph={<Escape org_nr="886813082"/>}/>
                 </div>
                 <div className="column-frames">
-                    <Frame overskrift="Lakselus" nøkkeltall_en={<Nokkeltall id={id}/>} nøkkeltall_to={<Nokkeltall id={id}/>} graph={<Lice org_nr="886813082"/>}/>
+                    <Frame overskrift="Lakselus" nøkkeltall_en={<Nokkeltall id={id} kpi="Lakselus_bedrift"/>} nøkkeltall_to={<Nokkeltall id={id} kpi="Lakselus_bransje"/>} graph={<Lice org_nr="886813082"/>}/>
                 </div>
                 <div className="column-frames">
-                    <Frame overskrift="Fôrproduksjon" nøkkeltall_en={<Nokkeltall id={id}/>} nøkkeltall_to={<Nokkeltall id={id}/>}/>
+                    <Frame overskrift="Fôrproduksjon" nøkkeltall_en={<Nokkeltall id={id} kpi="Forproduksjon_bedrift"/>} nøkkeltall_to={<Nokkeltall id={id} kpi="Forproduksjon_bransje"/>}/>
                 </div>
                 <div className="column-frames">
-                    <Frame overskrift="CO2" nøkkeltall_en={<Nokkeltall id={id}/>} nøkkeltall_to={<Nokkeltall id={id}/>}/>
+                    <Frame overskrift="CO2" nøkkeltall_en={<Nokkeltall id={id} kpi="CO2_bedrift"/>} nøkkeltall_to={<Nokkeltall id={id} kpi="CO2_bransje"/>}/>
                 </div>
             </div>
         </div>
--- Frontend/portal/src/Components/Nokkeltall/Nokkeltall.js
+++ Frontend/portal/src/Components/Nokkeltall/Nokkeltall.js
@@ -7,8 +7,31 @@ function Nokkeltall (props) {
     const transform_kpi = (kpi) => {
 
         switch(kpi) {
-            case "gjennomsnittlig dødlighet": 
-                return "Dødlighets gjennomsnitt";
+            //Dødlighet
+            case "Dødlighet_bedrift": 
+                return "Bedriften hadde gjennomsnittlig";
+            case "Dødlighet_bransje": 
+                return "Bransjen hadde i gjennomsnitt";
+            //Lakselus
+            case "Lakselus_bedrift": 
+                return "Bedriften hadde totalt";
+            case "Lakselus_bransje": 
+                return "Bransjen hadde totalt";
+            //Rømninger
+            case "Rømninger_bedrift": 
+                return "Bedriften hadde totalt";
+            case "Rømninger_bransje": 
+                return "Bransjen hadde totalt";
+            //Forproduksjon
+            case "Forproduksjon_bedrift": 
+                return "Bedriften slapp i gjennomsnitt ut";
+            case "Forproduksjon_bransje": 
+                return "Bransjen slapp i gjennomsnitt ut";
+            //CO2
+            case "CO2_bedrift": 
+                return "Bedriften slapp i gjennomsnitt ut";
+            case "CO2_bransje": 
+                return "Bransjen slapp i gjennomsnitt ut";
             default:
                 return "kunne ikke finne";
             }
