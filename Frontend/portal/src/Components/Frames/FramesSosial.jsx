03268a5a2ee579a3ab6268fab6ae5a9c8ef27675
sannagrosoy <108658771+sannagrosoy@users.noreply.github.com>
2022-08-02 12:03:54 +0200

    Css kjonnsgraf

--- Frontend/portal/src/Components/Frames/FramesSosial.jsx
+++ Frontend/portal/src/Components/Frames/FramesSosial.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 import Frame from './Frame';
-import Nokkeltall from '../Nokkeltall/Nokkeltall';
+import Nokkeltall from '../Nokkeltall/Nokkeltall'
+import NokkeltallFly from '../Nokkeltall/NokkeltallFly';
 import Kjonngraf from '../../Graphs/Kjonnsfordeling/kjonngraf';
 
 function FramesSosial() {
@@ -11,13 +12,19 @@ function FramesSosial() {
             <div className="container-frames">
             <div className="row-frames">
                 <div className="column-frames">
-                    <Frame overskrift= "Kjønnsfordeling" graph={<Kjonngraf/>} tekst={"Dette diagrammet viser andelen menn og kvinner i selskapet målt i prosent"} />
+                    <Frame overskrift= "Kjønnsfordeling" 
+                            graph={<Kjonngraf/>} 
+                            tekst={"Dette diagrammet viser andelen menn og kvinner i selskapet målt i prosent"} />
                 </div>
                 <div className="column-frames">
-                    <Frame overskrift="Arealbruk" tekst={"Disse tallene viser hvor stort areal hvert selskap benytter målt i m2"}/>
+                    <Frame overskrift="Arealbruk" 
+                            nøkkeltall_en={<NokkeltallFly id={id} kpi3="Arealbruk_bedrift" kpi4="Arealbruk_bedrift"/>}
+                            nøkkeltall_to={<NokkeltallFly id={id} kpi3="Arealbruk_bransje" kpi4="Arealbruk_bransje"/>}
+                            tekst={"Disse tallene viser hvor stort areal hvert selskap benytter målt i m2"}/>
                 </div>
                 <div className="column-frames">
-                    <Frame overskrift="Deltidsprosent" tekst={"Grafen viser antall arbeidere i ufrivillig deltid målt i prosent"}/>
+                    <Frame overskrift="Deltidsprosent" 
+                            tekst={"Grafen viser antall arbeidere i ufrivillig deltid målt i prosent"}/>
                 </div>
             </div>
         </div>
