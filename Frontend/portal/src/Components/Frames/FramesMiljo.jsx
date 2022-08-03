0b4c7fdd13a4f2e424572d33b6da60ec8f67efb2
sannagrosoy <108658771+sannagrosoy@users.noreply.github.com>
2022-08-03 12:43:40 +0200

    changed

--- Frontend/portal/src/Components/Frames/FramesMiljo.jsx
+++ Frontend/portal/src/Components/Frames/FramesMiljo.jsx
@@ -31,8 +31,9 @@ function FramesMiljo() {
                         nøkkeltall_to={<Nokkeltall miljo_over="Dødlighet_bransje" id={id} miljo_under="Dødlighet_bedrift"/>} 
                         tekst={"Grafen viser prosentandelen av produsert laks som har dødd under produksjon fra 2017-2021"} 
                         graph={<Death org_nr={id}/>}
-                        dropDown={"Dødlighet er andelen av selskapets produksjon"}
-                        show={"som har dødd under oppdrettsprosessen. Tallet er samlet for alle selskapets lokasjoner."}/>
+                        dropDown={"Dødlighet forklart"}
+                        show={"Dødlighet er andelen av selskapets produksjon som har dødd under oppdrettsprosessen. Tallet er samlet for alle selskapets lokasjoner."}
+                        kilde={"Kilde: Konstruert tall"}/>
 
                 </div>
                 <div className="column-frames">
@@ -41,8 +42,9 @@ function FramesMiljo() {
                         nøkkeltall_to={<Nokkeltall id={id} miljo_over="Rømninger_bransje" miljo_under="Rømninger_bedrift"/>} 
                         tekst={"Grafen viser antall rømninger per år fra 2017-2021"} 
                         graph={<Escape org_nr={id}/>}
-                        dropDown={"Rømningstallet viser antall røminger selskapet"}
-                        show={"Rømninger innebærer at fisken har kommet ut av merden. Eventuelle redningsaksjoner er ikke tatt hensyn til i dette diagrammet."}/>
+                        dropDown={"Rømninger forklart"}
+                        show={"Rømningstallet viser antall rømniger selskapet har hatt i løpet av et år. Rømninger innebærer at fisken har kommet ut av merden og sluppet ut i havet. Eventuelle redningsaksjoner er ikke tatt hensyn til i dette diagrammet."}
+                        kilde={"Kilde: https://www.barentswatch.no/fiskehelse/"}/>
                         
                 </div>
                 <div className="column-frames">
@@ -51,8 +53,9 @@ function FramesMiljo() {
                         nøkkeltall_to={<Nokkeltall id={id} miljo_over="Lakselus_bransje" miljo_under="Lakselus_bedrift"/>} 
                         tekst={"Grafen viser gjennomsnittlig lakselus per fisk fra 2017-2021"} 
                         graph={<Lice org_nr={id}/>}
-                        dropDown={"Lusetallene er gjennomsnittlig hunnlus per fisk."}
-                        show={"Lusetall er gjennomsnittlig hunnlus per fisk. Tallene er basert på data per uke for hver lokalitet og har blitt aggregert på årsnivå. Diagrammet viser bedriften sitt lusetall, gjennomsnittlig lusetall for bransjen, og tillatt grense for lakselus."}/>
+                        dropDown={"Lakselus forklart"}
+                        show={"Lusetall er gjennomsnittlig hunnlus per fisk. Tallene er basert på data som innrapporteres hver uke på lokasjonsnivå. Dataen er samlet på årsnivå i diagrammet. Diagrammet viser bedriftens lusetall, gjennomsnittlig lusetall for bransjen, og tillatt grense for lakselus."}
+                        kilde={"Kilde: https://www.barentswatch.no/fiskehelse/"}/>
                 </div>
                 <div className="column-frames">
                     <Frame overskrift={"Co2 Fôrproduksjon"} 
@@ -60,17 +63,19 @@ function FramesMiljo() {
                         nøkkeltall_to={<NokkeltallFly id={id} fly_over="Fôrproduksjon_bransje" fly_under="Fôrproduksjon_bransje"/>} 
                         tekst={"Tallene er basert på beregnet CO2-ekvivalent fra fôrforbruket i 2022"} 
                         graph={<Feed org_nr={id}/>}
-                        dropDown={"CO2-utslippet er regnet ut ved å gange et fast"}
-                        show={"klimaavtrykk per kilo fôr, med fôrforbruket per kilo laks (eFcr) og selskapets totale produksjon av laks. Det tas forbehold om at tallet er et estimat basert på et fast utslipp per kilo for, og at selskapet benytter samme fôrprodusent for hele produksjonen."}/>
+                        dropDown={"Co2 fôrproduksjon forklart"}
+                        show={"CO2-utslippet er regnet ut ved å gange et fast klimaavtrykk per kilo fôr, med fôrforbruket per kilo laks (eFcr) og selskapets totale produksjon av laks. Det tas forbehold om at tallet er et estimat basert på et fast utslipp per kilo for, og at selskapet benytter samme fôrprodusent for hele produksjonen."}
+                        kilde={"Kilde: Konstruert tall basert på https://www.sintef.no/contentassets/25338e561f1a4270a59ce25bcbc926a2/report-carbon-footprint-norwegian-seafood-products-2017_final_040620.pdf/"}/>
                 </div>
                 <div className="column-frames">
-                    <Frame overskrift={"Co2 produksjon" }
+                    <Frame overskrift={"Co2 Produksjon" }
                         nøkkeltall_en={<NokkeltallFly id={id} fly_over="CO2_bedrift" fly_under="CO2_bedrift"/>} 
                         nøkkeltall_to={<NokkeltallFly id={id} flyo_over="CO2_bransje" fly_under="CO2_bransje"/>} 
                         tekst={"Tallene er basert på beregnet CO2-ekvivalent fra produksjonen i 2022"} 
                         graph={<Production org_nr={id}/>}
-                        dropDown={"CO2 utslippet fra produksjon er beregnet etter"}
-                        show={"drivstofforbruk under utvalgte aktiviteter i produksjonsprosessen. Disse aktivitetene er bruk av brønnbåter, produksjonsfarmer og servicebedriftbruk. Det benyttes en fast sats for utslipp  per liter brukt på 2.66 CO2 ekvivalenter."}/>
+                        dropDown={"Co2 produkjson forklart"}
+                        show={"CO2 utslippet fra produksjon er beregnet etter drivstofforbruk under utvalgte aktiviteter i produksjonsprosessen. Disse aktivitetene er bruk av brønnbåter, produksjonsfarmer og servicebedriftbruk. Det benyttes en fast sats for utslipp  per liter brukt på 2.66 CO2 ekvivalenter."}
+                        kilde={"Kilde: Konstruert tall basert på https://www.sintef.no/contentassets/25338e561f1a4270a59ce25bcbc926a2/report-carbon-footprint-norwegian-seafood-products-2017_final_040620.pdf/"}/>
 
                 </div>
             </div>
