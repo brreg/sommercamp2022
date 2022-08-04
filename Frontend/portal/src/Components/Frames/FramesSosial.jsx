6c3977e1beb0896c5e65028f82f52882837cc3de
sannagrosoy <108658771+sannagrosoy@users.noreply.github.com>
2022-08-04 11:59:31 +0200

    nøkkeltall ferdig

--- Frontend/portal/src/Components/Frames/FramesSosial.jsx
+++ Frontend/portal/src/Components/Frames/FramesSosial.jsx
@@ -24,8 +24,9 @@ function FramesSosial() {
                 </div>
                 <div className="column-frames">
                     <Frame overskrift="Arealbruk" 
-                            nøkkeltall_en={<NokkeltallAreal id={id} areal_over="Arealbruk_bedrift" areal_under="Arealbruk_bedrift"/>}
-                            nøkkeltall_to={<NokkeltallAreal id={id} areal_over="Arealbruk_bransje" areal_under="Arealbruk_bransje"/>}
+                            nøkkeltall_en={<NokkeltallAreal id={id} areal_over="Arealbruk_bedrift" areal_under="Arealbruk_bedrift" areal_tall="Arealbruk_bedrift" areal_ball="Fotball_bedrift"/>}
+                            nøkkeltall_to={<NokkeltallAreal id={id} areal_over="Arealbruk_bransje" areal_under="Arealbruk_bransje" areal_tall="Arealbruk_bransje" areal_ball="Fotball_bransje"/>}
+                            graph={<Kjonngraf id={id}/>} 
                             tekst={"Disse tallene viser hvor stort areal hvert selskap benytter målt i m2"}
                             dropDown={"Arealbruk forklart"}
                             show={"Arealbruk viser hvor stort område selskapet benytter for å drive med oppdrettsaktivitet."}
