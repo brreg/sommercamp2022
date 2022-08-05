f053861344e5aa0b855953b5021bb26007d00997
sannagrosoy <108658771+sannagrosoy@users.noreply.github.com>
2022-08-05 09:42:21 +0200

    fix

--- Frontend/portal/src/Components/Frames/FramesSosial.jsx
+++ Frontend/portal/src/Components/Frames/FramesSosial.jsx
@@ -27,7 +27,6 @@ function FramesSosial() {
                     <Frame overskrift="Arealbruk" 
                             nøkkeltall_en={<NokkeltallAreal id={id} areal_over="Arealbruk_bedrift" areal_under="Arealbruk_bedrift" areal_tall="Arealbruk_bedrift" areal_ball="Fotball_bedrift"/>}
                             nøkkeltall_to={<NokkeltallAreal id={id} areal_over="Arealbruk_bransje" areal_under="Arealbruk_bransje" areal_tall="Arealbruk_bransje" areal_ball="Fotball_bransje"/>}
-                            graph={<Kjonngraf id={id}/>} 
                             tekst={"Disse tallene viser hvor stort areal hvert selskap benytter målt i m2"}
                             dropDown={"Arealbruk forklart"}
                             show={"Arealbruk viser hvor stort område selskapet benytter for å drive med oppdrettsaktivitet."}
