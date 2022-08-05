66f66662e72e5ae74a780add18e522cb9e4fb69e
andreaholmboe <andreaholmboe@yahoo.no>
2022-08-05 15:03:53 +0200

    css update

--- Frontend/portal/src/Components/Navbar/Navbar.js
+++ Frontend/portal/src/Components/Navbar/Navbar.js
@@ -1,25 +1,39 @@
-import './Navbar.css'
-import React from 'react';
+import "./Navbar.css";
+import React from "react";
 
 const NavBar = () => {
-    return (
-        <div>
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-            <nav className="navbar">
-                <div className="navbar-img">
-                        <a href="/"><img src={require('./images/BR_logo-bokmaal_hvit.png')} className="image-nav" alt="logo"></img></a>
-                </div>
-                        <a className="nav-a-left" href="/Bedrifter/Miljo" >Virksomheter</a>
-                        <a className="nav-a-left" href="">Bærekraft</a>
-                        <a className="nav-a-left" href="">Regelverk</a>
-                        <a className="nav-a-right" href="">Meny</a>
-                        <a className="nav-a-right" href="">Språk</a>
-                        
-                        
-
-            </nav>
-       </div>
-    )
-}
+  return (
+    <div>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      ></meta>
+      <nav className="navbar">
+        <a href="/">
+          <img
+            src={require("./images/BR_logo-bokmaal_hvit.png")}
+            className="image-nav navbar-img"
+            alt="logo"
+          ></img>
+        </a>
+        <a className="nav-a nav-left nav-virksomheter" href="/Bedrifter/Miljo">
+          Virksomheter
+        </a>
+        <a className="nav-a nav-left" href="">
+          Bærekraft
+        </a>
+        <a className="nav-a nav-left" href="">
+          Regelverk
+        </a>
+        <a className="nav-a nav-right" href="">
+          Meny
+        </a>
+        <a className="nav-a nav-right" href="">
+          Språk
+        </a>
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
